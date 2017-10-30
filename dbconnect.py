1d0ad22d07c49e7c2d29825aa7ddc8ff3ae00ea3
Robin246 <robinleesuns13@gmail.com>
2017-10-29 18:26:03 -0700

    Create working register form page

--- dbconnect.py
+++ dbconnect.py
@@ -1,7 +1,7 @@
 import MySQLdb
-
+# hint how to create python anywhere connection code with mysql https://www.pythonanywhere.com/forums/topic/11305/
 def connection():
-    conn = MySQLdb.connect(host="localhost",
+    conn = MySQLdb.connect(host="automataanywhere.mysql.pythonanywhere-services.com",
                            user = "automataanywhere",
                            passwd = "niermankind6o",
                            db = "automataanywhere$pythonprogramming"
--- learnFlask.py
+++ learnFlask.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, flash, request, url_for, redirect
+from flask import Flask, render_template, flash, request, url_for, redirect, session
 from content_management import Content
 
+#pycharms way of using FLask-WTForm
+from flask_wtf import Form
+from wtforms import StringField, BooleanField, validators, PasswordField
+from passlib.hash import sha256_crypt
+
+from MySQLdb import escape_string as thwart
+
 from dbconnect import connection
+import gc
 
 TOPIC_DICT = Content()
 
@@ -48,12 +56,50 @@ def loginpage():
         # flash(e) # delete this when done
         return render_template("login.html", error = error)
 
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=20)])
+    email = StringField('Email Address', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [validators.DataRequired(),
+                                          validators.EqualTo('confirm', message="Passswords must match.")])
+    confirm = PasswordField('Repeat Password')
+
+    accept_tos = BooleanField('I accept the <a href="/tos/">Terms of Service</a> and the <a href="/privacy/">Privacy Notice</a> (Last updated Jan 15 2015)', [validators.DataRequired()])
+
+
+
 
 @app.route('/register/', methods = ['GET','POST'])
 def register_page():
     try:
-        c, conn = connection()
-        return("okay")
+        form = RegistrationForm(request.form)
+
+        if request.method == "POST" and form.validate():
+            username = form.username.data
+            email = form.email.data
+            password = sha256_crypt.encrypt((str(form.password.data)))
+            c, conn = connection()
+
+            x = c.execute("SELECT * FROM users WHERE username = '{}'".format(thwart(username)))
+
+            if int(x) > 0:
+                flash("That username is already taken, please choose another")
+                return render_template('register.html', form = form)
+            else:
+                c.execute("INSERT INTO users (username, password, email, tracking) VALUES (%s, %s, %s, %s)",
+                          (thwart(username), thwart(password), thwart(email), thwart("/introduction-to-python-programming/")))
+
+                conn.commit()
+                flash("Thanks for registering!")
+                c.close()
+                conn.close()
+                gc.collect()
+
+                session['logged_in'] = True
+                session['username'] = username
+                return redirect(url_for('dashboard'))
+
+        return render_template('register.html', form=form)
+
     except Exception as e:
         return(str(e))
 
